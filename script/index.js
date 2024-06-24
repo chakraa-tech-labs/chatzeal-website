30f753ac21a7c138816b5d1b8e32476e0072b459
Tamilarasu <tamilarasukvp54@gmail.com>
2024-06-24 12:48:22 +0530

    change photes and animations.

--- script/index.js
+++ script/index.js
@@ -1,12 +1,8 @@
 // JavaScript code
 
 var background = document.getElementById("background-div");
-var form_background = document.getElementById("from-close-div");
 var successMessage = document.getElementById("successMessage");
 var body = document.getElementById("body");
-var nav_bar = document.getElementById("nav-links");
-var nav_form = document.getElementById("nav-links-form");
-// var form = document.getElementById("myForm");
 
 document.querySelectorAll("a").forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
@@ -16,28 +12,6 @@ document.querySelectorAll("a").forEach((anchor) => {
     });
   });
 });
-
-function FormOpen() {
-  // alert("hai");
-  console.log(nav_bar);
-  nav_bar.style.display = "none";
-  form_background.style.display = "block";
-  nav_form.style.transform = "scalex(1)";
-  // body.style.overflow = "hidden";
-}
-
-function FormClose() {
-  // alert("hai");
-  setTimeout(function () {
-    nav_bar.style.display = "block";
-  }, 900);
-  form_background.style.display = "none";
-  nav_form.style.transform = "scalex(0)";
-
-  // body.style.overflow = "auto";
-  // background.style.display = "none";
-  // form.style.transform = "scale(0)";
-}
 
 function showSuccessMessage(e) {
   e.preventDefault();
@@ -58,18 +32,15 @@ function showSuccessMessage(e) {
   background.style.transform = "scalex(1)";
   body.style.overflow = "hidden";
 
-  // //Close Message
+  // Close Message
   setTimeout(function () {
     successMessage.style.transform = "scale(0)";
-    background.style.transform = "scalex(0)";
-    body.style.overflow = "auto";
-  }, 2500);
 
-  setTimeout(function () {
-    nav_bar.style.display = "block";
-  }, 900);
-  form_background.style.display = "none";
-  nav_form.style.transform = "scalex(0)";
+    setTimeout(function () {
+      background.style.transform = "scaleX(0)";
+      body.style.overflow = "auto";
+    }, 500);
+  }, 2500);
 }
 
 function smoothScrollTo(targetId) {
