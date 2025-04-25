dcf9553c7aafbbba2ebdf7db955102d779dad1ce
Tamilarasu <tamilarasukvp54@gmail.com>
2025-04-25 16:00:22 +0530

    Remove components js file.

--- script/features.js
+++ script/features.js
@@ -1,17 +1,17 @@
 // a Tag Smooth Scroll
 document.querySelectorAll(".sub-header-box .sub-header a").forEach((anchor) => {
-    anchor.addEventListener("click", function (e) {
-      e.preventDefault();
-      const target = document.querySelector(this.getAttribute("href"));
-      const offset = 10;
-      const bodyRect = document.body.getBoundingClientRect().top;
-      const elementRect = target.getBoundingClientRect().top;
-      const elementPosition = elementRect - bodyRect;
-      const offsetPosition = elementPosition - offset;
-  
-      window.scrollTo({
-        top: offsetPosition,
-        behavior: "smooth",
-      });
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    const offset = 10;
+    const bodyRect = document.body.getBoundingClientRect().top;
+    const elementRect = target.getBoundingClientRect().top;
+    const elementPosition = elementRect - bodyRect;
+    const offsetPosition = elementPosition - offset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: "smooth",
     });
   });
+});
